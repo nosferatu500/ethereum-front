3bf0c2daec76e992ca770aaf54a4c3007ff55559
bik <bik@sfxdx.ru>
2018-02-07 15:54:37 +0700

    del version del windows/386

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 
 ### build backend
 dir=`pwd`
-version=0.0.1
 
-xgo -out efront-v${version} --targets=windows/amd64,windows/386,darwin/amd64,linux/amd64 .
-mv -f ${dir}/efront-v${version}-* bin
+xgo -out efront --targets=windows/amd64,darwin/amd64,linux/amd64 .
+mv -f ${dir}/efront-* bin
 echo "assembly complete"
